70eb6419bb15f46726829a08a8371319ecc1d497
GuillermoGonzalez11 <139256299+GuillermoGonzalez11@users.noreply.github.com>
2023-07-16 19:43:48 -0300

    tareaa

--- tareas.js
+++ tareas.js
@@ -17,7 +17,7 @@ let vacío= null
 //Crea una variable indefinido y asígnale un valor de tipo undefined.
 let indefinido= undefined
 //Crea una variable arreglo y asígnale un arreglo con varios valores de diferentes tipos de datos.
-
+const miArreglo = [1, "Hola", true, {nombre: "Juan", edad: 25}];
 //Imprime en la consola el valor de la variable número y el tipo de dato que almacena.
 console.log(a,typeof a)
 //Imprime en la consola el valor de la variable texto y el tipo de dato que almacena.
@@ -25,6 +25,9 @@ console.log(miVariable,typeof miVariable)
 //Imprime en la consola el valor de la variable verdadero y el tipo de dato que almacena.
 console.log(falso,typeof falso)
 //Imprime en la consola el valor de la variable falso y el tipo de dato que almacena.
+const falso = false;
+console.log(fals); // Imprime "false" en la consola
+console.log(typeof falso); // Imprime "boolean" en la consola
 
 //Imprime en la consola el valor de la variable nulo y el tipo de dato que almacena.
 console.log(vacío, typeof vacío)
@@ -107,7 +110,18 @@ function isPar(numero){
 console.log(isPar(9))
 console.log(isPar(8))
 //Escribe una función que tome un array de números como argumento y devuelva la suma de sus elementos.
-
+function sumaElementos(arrayNumeros) {
+    let suma = 0;
+    for (let i = 0; i < arrayNumeros.length; i++) {
+      suma += arrayNumeros[i];
+    }
+    return suma;
+  }
+  
+  const arrayNumeros = [2, 4, 6, 8, 10];
+  const suma = sumaElementos(arrayNumeros);
+  console.log(suma);
+  
 //Escribe una función que tome un array de strings como argumento y devuelva la longitud del string más largo.
 let miArray = [
     "hola",
@@ -121,7 +135,21 @@ for (let i = 0; i<tamaño; i++){
 }
 
 //Escribe una función que tome un objeto como argumento y devuelva un array con los valores de todas sus propiedades.
-
+function valoresPropiedades(objeto) {
+    const arrayValores = [];
+    for (let propiedad in objeto) {
+      arrayValores.push(objeto[propiedad]);
+    }
+    return arrayValores;
+  }
+  
+  const objeto = {
+    nombre: "Juan",
+    edad: 30,
+    ciudad: "Asunción"
+  };
+  const arrayValores = valoresPropiedades(objeto);
+console.log(arrayValores);
 
 
 
@@ -129,15 +157,55 @@ for (let i = 0; i<tamaño; i++){
 //Vectores
 
 //Escribe una función que tome un array de números como argumento y devuelva el número más grande.
-
+function encontrarNumeroMasGrande(numeros) {
+    let numeroMasGrande = numeros[0];
+    for (let i = 1; i < numeros.length; i++) {
+      if (numeros[i] > numeroMasGrande) {
+        numeroMasGrande = numeros[i];
+      }
+    }
+    return numeroMasGrande;
+  }
+  
 //Escribe una función que tome un array de números como argumento y devuelva un array con los números ordenados de menor a mayor.
+function ordenarNumeros(numeros) {
+    return numeros.sort((a, b) => a - b);
+  } const numerosDesordenados = [3,1,4,1,5,9,2,6,5,3,5];
+  const numerosOrdenados = ordenarNumeros(numerosDesordenados);
+  console.log(numerosOrdenados);
 
 //Escribe una función que tome dos arrays como argumentos y devuelva un array que contenga los elementos comunes entre ambos.
+function elementosComunes(array1, array2) {
+  return array1.filter(element => array2.includes(element));
+}
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+const elementosEnComun = elementosComunes(array1, array2);
+console.log(elementosEnComun);
 
 //Escribe una función que tome un array de strings como argumento y devuelva un nuevo array que contenga los strings convertidos a mayúsculas.
+function convertirAMayusculas(strings) {
+    return strings.map(string => string.toUpperCase());
+  }
+  
+  const strings = ['hola', 'mundo', 'esto', 'es', 'una', 'prueba'];
+  const stringsEnMayusculas = convertirAMayusculas(strings);
+  console.log(stringsEnMayusculas);  
 
 //Escribe una función que tome un array de números como argumento y devuelva un nuevo array que contenga la suma acumulativa de sus elementos.
+function sumarAcumulativo(array) {
+  const arrayAcumulado = [];
+  let sumaAcumulada = 0;
+  for (let i = 0; i < array.length; i++) {
+    sumaAcumulada += array[i];
+    arrayAcumulado.push(sumaAcumulada);
+  }
+  return arrayAcumulado;
+}
 
+const array = [1, 2, 3, 4, 5];
+const arrayAcumulado = sumarAcumulativo(array);
+console.log(arrayAcumulado);
 
 
 
@@ -154,11 +222,32 @@ while (i < 6) {
     i++;
 }
 //Crea un vector con los números del 1 al 5. Escribe un bucle for que imprima en la consola cada uno de los elementos del vector.
+var  numeros = [1,2,3,4,5];
+numeros.forEach(function(numero){
+    console.log(numero);
 
+});
 //Crea una función que reciba un número n y devuelva la suma de todos los números del 1 al n. Utiliza un bucle for para calcular la suma y muestra el resultado en la consola.
-
+function sumaHastaN(n){
+    var suma = 0;
+    for (var i = 1; i<=n;i++){
+        suma+=i;
+    }
+    return suma;
+}
+console.log(sumaHastaN(10));
 //Crea una función que reciba un vector y devuelva la suma de todos sus elementos. Utiliza un bucle while para calcular la suma y muestra el resultado en la consola.
 
+function sumaVector(vector){
+    var suma = 0;
+    var i= 0;
+    while(i <vector.length){
+        suma += vector[i]
+        i++;
+    }
+    return suma;
+}
+console.log(sumaVector([1,2,3,4,5]));
 
 
 
@@ -167,10 +256,11 @@ while (i < 6) {
 
 //Crea un archivo HTML con un botón y un div vacío. Usa JavaScript para agregar un mensaje al div cuando se hace clic en el botón.
 
+
 //Crea una función que cambie el color de fondo de un elemento HTML a rojo cuando el mouse se mueve sobre él. Agrega esta función como un listener de evento para el div del ejercicio anterior.
 
 //Crea una función que cambie el tamaño de fuente de un elemento HTML cuando se presiona una tecla en el teclado. Agrega esta función como un listener de evento para el div del ejercicio anterior.
 
-//Crea una función que cambie el contenido de un elemento HTML cuando se hace doble clic sobre él. Agrega esta función como un listener de evento para el div del ejercicio anterior.
+//Crea una función que cambie el tamaño de fuente de un elemento HTML cuando se presiona una tecla en el teclado. Agrega esta función como un listener de evento para el div del ejercicio anterior.
 
 //Crea una función que cambie la imagen de fondo de un elemento HTML cuando se carga la página. Agrega esta función como un listener de evento para el body del documento.
